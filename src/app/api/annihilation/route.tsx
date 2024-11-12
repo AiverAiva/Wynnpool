0ae44edbe0b5b7c7b661d6805c08a62560fe2160
AiverAiva <43096905+AiverAiva@users.noreply.github.com>
2024-11-12 16:03:16 +0800

    revert for testing

--- src/app/api/annihilation/route.tsx
+++ src/app/api/annihilation/route.tsx
@@ -7,27 +7,23 @@ const LOCAL_DATA_PATH = path.join(process.cwd(), 'src', 'data', 'annihilation.js
 
 export async function GET() {
   try {
-    // Check if local data file exists
-    let data;
-    if (fs.existsSync(LOCAL_DATA_PATH)) {
-      const localData = JSON.parse(fs.readFileSync(LOCAL_DATA_PATH, 'utf-8'));
-      const currentTime = Date.now();
+    // let data;
+    // if (fs.existsSync(LOCAL_DATA_PATH)) {
+    //   const localData = JSON.parse(fs.readFileSync(LOCAL_DATA_PATH, 'utf-8'));
+    //   const currentTime = Date.now();
 
-      // If the current datetime in local data is in the future, use it
-      if (localData.current.datetime_utc > currentTime) {
-        data = localData;
-      }
-    }
+    //   if (localData.current.datetime_utc > currentTime) {
+    //     data = localData;
+    //   }
+    // }
 
-    // If data is not set (either file doesn't exist or datetime passed), fetch new data
-    if (!data) {
-      const response = await fetch(DATA_URL);
-      data = await response.json();
+    // if (!data) {
+    //   const response = await fetch(DATA_URL);
+    //   data = await response.json();
 
-      // Save the fetched data to local file
-      fs.writeFileSync(LOCAL_DATA_PATH, JSON.stringify(data, null, 2), 'utf-8');
-    }
-
+    //   fs.writeFileSync(LOCAL_DATA_PATH, JSON.stringify(data, null, 2), 'utf-8');
+    // }
+    const data = JSON.parse(fs.readFileSync(LOCAL_DATA_PATH, 'utf-8'));
     // Send response
     return NextResponse.json(data, { status: 200 });
   } catch (error) {
