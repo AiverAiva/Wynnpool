b6867e0da9691f23204293f9fbc82210f7b0ab86
AiverAiva <43096905+AiverAiva@users.noreply.github.com>
2024-11-13 10:48:23 +0800

    feat: add lootrun page

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,9 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  images: {
+    domains: ['cdn.wynncraft.com', 'nori.fish'], // might remove this later
+  },
 };
 
 export default nextConfig;
