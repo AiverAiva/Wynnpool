a16e22856186919c89bad716e6d164612f47a05c
AiverAiva <43096905+AiverAiva@users.noreply.github.com>
2024-11-27 05:59:52 +0800

    refactor: [#ce85402] missed on commit

--- src/components/custom/item-display.tsx
+++ src/components/custom/item-display.tsx
@@ -122,7 +122,6 @@
 //     </Card>
 //   )
 // }
-
 import { Card, CardContent, CardDescription, CardHeader, CardTitle } from "@/components/ui/card"
 import { Badge } from "@/components/ui/badge"
 import { Separator } from "@/components/ui/separator"
