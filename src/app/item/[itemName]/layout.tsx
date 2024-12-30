76b221b4e9b378b4f2a849056ca67655753e2639
AiverAiva <43096905+AiverAiva@users.noreply.github.com>
2024-12-31 04:01:20 +0800

    feat: metadata for item

---
+++ src/app/item/[itemName]/layout.tsx
@@ -0,0 +1,52 @@
+import { getImageSrc } from "@/components/custom/WynnIcon";
+import { Item } from "@/types/itemType";
+
+export default function PlayerLayout({
+    children, // will be a page or nested layout
+}: {
+    children: React.ReactNode
+}) {
+    return (
+        <section>
+            {children}
+        </section>
+    )
+}
+
+export async function generateMetadata({ params }: { params: Promise<{ itemName: string }> }) {
+    const { itemName } = await params;
+
+    const res = await fetch(`${process.env.BASE_URL}/api/item/${itemName}`)
+    if (!res.ok) {
+        return {
+            title: 'Item Not Found',
+            description: `The item "${itemName}" could not be found.`,
+        };
+    }
+
+    const itemData: Item = await res.json();
+    const iconUrl: string = getImageSrc(itemData)
+
+    return {
+        title: `${itemData.internalName} - Item Info`,
+        description: `View item information for ${itemData.internalName} on our platform.`,
+        openGraph: {
+            title: `${itemData.internalName} - Item Info`,
+            description: `View item information for ${itemData.internalName}.`,
+            url: `${process.env.BASE_URL}/item/${itemData.internalName}`,
+            images: [
+                {
+                    url: iconUrl, 
+                    alt: `${itemData.internalName}`,
+                },
+            ],
+        },
+
+        twitter: {
+            card: 'summary_large_image',
+            title: `${itemData.internalName} - Player Stats`,
+            description: `Detailed player statistics for ${itemData.internalName}.`,
+            images: [iconUrl], 
+        },
+    };
+}
