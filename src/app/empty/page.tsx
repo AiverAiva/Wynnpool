39e80124707da8b38acea758001b89b9758f6f0a
AiverAiva <43096905+AiverAiva@users.noreply.github.com>
2025-04-16 03:29:34 +0800

    chore: add a sample page for copypasting :3

---
+++ src/app/empty/page.tsx
@@ -0,0 +1,10 @@
+'use client'
+
+export default function ExamplePage() {
+    return (
+        <div className="min-h-screen flex items-center justify-center bg-background">
+            <div className="mt-[80px]" />
+            {/* <main className="container mx-auto px-4 py-8 max-w-screen-lg"></main> */}
+        </div>
+    )
+}
