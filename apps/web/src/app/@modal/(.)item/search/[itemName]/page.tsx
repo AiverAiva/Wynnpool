feb0c80942fc8209f10b57cfd7690cb8c37548f5
AiverAiva <43096905+AiverAiva@users.noreply.github.com>
2025-07-20 00:15:46 +0800

    fix(web): item modal in search not syncing with item page

--- apps/web/src/app/@modal/(.)item/search/[itemName]/page.tsx
+++ apps/web/src/app/@modal/(.)item/search/[itemName]/page.tsx
@@ -1,9 +1,25 @@
-import { Modal } from "./modal";
+'use client'
 
-export default async function ItemDetailModal({ params }: any) {
-    const itemName = (await params).itemName;
+import { useParams, useRouter } from "next/navigation";
+import ItemComponent from "@/app/item/[itemName]/ItemComponent";
+import { ScrollArea } from "@/components/ui/scroll-area";
+import { DialogContent, DialogHeader, DialogTitle } from "@/components/ui/dialog";
+import { Dialog } from "@radix-ui/react-dialog";
+
+export default async function ItemDetailModal() {
+    const { itemName } = useParams<{ itemName: string }>();
+    const router = useRouter();
 
     return (
-        <Modal itemName={itemName} />
+        <Dialog open onOpenChange={() => router.back()}>
+            <DialogContent>
+                <DialogHeader>
+                    <DialogTitle>Item details</DialogTitle>
+                </DialogHeader>
+                <ScrollArea className='h-[70vh]'>
+                    <ItemComponent itemName={itemName} />
+                </ScrollArea>
+            </DialogContent>
+        </Dialog>
     );
 }
