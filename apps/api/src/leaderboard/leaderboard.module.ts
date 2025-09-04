5c1a7da724e0d00a489f4f4c86a6a18ab42d59d5
AiverAiva <43096905+AiverAiva@users.noreply.github.com>
2025-09-04 14:56:13 +0800

    feat(api): a proxy for wynncraft leaderboard api

--- apps/api/src/leaderboard/leaderboard.module.ts
+++ apps/api/src/leaderboard/leaderboard.module.ts
@@ -2,12 +2,14 @@ import { Module } from '@nestjs/common';
 import { MongooseModule } from '@nestjs/mongoose';
 import { LeaderboardController } from './leaderboard.controller';
 import { LeaderboardService } from './leaderboard.service';
+import { HttpModule } from '@nestjs/axios';
 import { GuildOnlineCountSchema } from '@shared/schemas/online-count.schema';
 import { GuildModule } from '../guild/guild.module';
 import { GuildSchema } from '@shared/schemas/guild.schema';
 
 @Module({
     imports: [
+        HttpModule,
         MongooseModule.forFeature([
             { name: 'guild_online_count', schema: GuildOnlineCountSchema },
             { name: 'guild_data', schema: GuildSchema },
