a2a6c272688725849479dfe5c7784fbc0f7abd03
AiverAiva <43096905+AiverAiva@users.noreply.github.com>
2025-12-13 01:53:25 +0800

    feat(web): Add credits page and link in footer
    Introduces a new credits page at /project/credit to acknowledge resources and inspirations used in Wynnpool. Updates the footer to include a link to the new credits page.

--- apps/web/src/components/layout/footer.tsx
+++ apps/web/src/components/layout/footer.tsx
@@ -42,6 +42,7 @@ export function Footer() {
 
                         <FooterSection title="Project">
                             <FooterLink href="/project/contributors">Contributors</FooterLink>
+                            <FooterLink href="/project/credit">Credits</FooterLink>
                             <FooterLink href="https://status.wynnpool.com">Service Status</FooterLink>
 
                             {/* <FooterLink href="/sponsor">Sponsors</FooterLink> */}
