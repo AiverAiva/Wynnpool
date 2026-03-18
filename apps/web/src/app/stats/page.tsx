df43d0b06fe8f7b8e8124b7a72458507cfa2776f
AiverAiva <43096905+AiverAiva@users.noreply.github.com>
2026-03-19 04:04:51 +0800

    chore(web): refactor festival of the blizzard card to spirit

--- apps/web/src/app/stats/page.tsx
+++ apps/web/src/app/stats/page.tsx
@@ -12,7 +12,7 @@ import Link from 'next/link'
 import { SeasonRatingCard } from '@/components/custom/season-rating-card'
 import { SeasonRatingLeaderboardModal } from '@/components/custom/season-rating-leaderboard-modal'
 import { LeaderboardCard } from '@/components/custom/leaderboard-card'
-import { FestivalOfTheBlizzardCard } from '@/components/custom/festival-of-the-blizzard-card'
+import { FestivalOfTheSpiritCard } from '@/components/custom/festival-of-the-spirit-card'
 
 interface Guild {
   rank: number;
@@ -96,7 +96,7 @@ export default function StatsPage() {
 
         <LeaderboardCard />
         <SeasonRatingCard onClick={() => setSeasonRatingModalOpen(true)} />
-        <FestivalOfTheBlizzardCard />
+        <FestivalOfTheSpiritCard />
         {/* </div> */}
       </div>
       <SeasonRatingLeaderboardModal open={seasonRatingModalOpen} onOpenChange={setSeasonRatingModalOpen} />
